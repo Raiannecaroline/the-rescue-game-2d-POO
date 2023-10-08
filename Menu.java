d3618b79f59ed2898f24ec71314b96c1fece14fd
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2023-10-08 17:24:07 -0300

    Criação da classe Menu e intro

---
+++ Menu.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends World
+{
+    private int tela = 1;
+    /**
+     * Constructor for objects of class Menu.
+     * 
+     */
+    public Menu()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(700, 390, 1);
+        setBackground("menu-01" + tela + ".png");
+        MundoIntro.timer = 1000;
+    }
+    public void act(){
+        String key = Greenfoot.getKey();
+        if(("s".equals(key) || "down".equals(key))){
+            if(tela == 3){tela = 1;}
+            else{tela++;}
+            setBackground("menu-01" + tela + ".png");
+        }
+        if(("w".equals(key) || "down".equals(key))){
+            if(tela == 1){tela = 3;}
+            else{tela--;}
+            setBackground("menu-01" + tela + ".png");
+        }
+        
+        if("enter".equals(key)){
+            switch(tela){
+                case 1:Greenfoot.setWorld(new Mundo1());break;                                     
+                case 2:Greenfoot.stop();break;
+            }
+        }   
+    }
+}
